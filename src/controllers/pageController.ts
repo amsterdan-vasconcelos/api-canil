ee3bc3e91eb2ac89fe50b5569c11e3d1f1bbe43f
Amsterdan Vasconcelos <amsterdan.contato@gmail.com>
2024-11-06 21:41:22 -0300

    feat: create controllers

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,30 +1,39 @@
 import { Request, Response } from "express";
+import { createMenuObject } from "../helpers/createMenuObject";
 
-export const home = (req: Request, res: Response) => {
-  res.json({
-    success: {
-      message: "Home",
+export const home = (_req: Request, res: Response) => {
+  res.render("pages/page", {
+    menu: createMenuObject("all"),
+    banner: {
+      title: "Todos os animais",
+      background: "allanimals.jpg",
     },
   });
 };
-export const dogs = (req: Request, res: Response) => {
-  res.json({
-    success: {
-      message: "Dogs",
+export const dogs = (_req: Request, res: Response) => {
+  res.render("pages/page", {
+    menu: createMenuObject("dogs"),
+    banner: {
+      title: "Todos os cachorros",
+      background: "banner_dog.jpg",
     },
   });
 };
-export const cats = (req: Request, res: Response) => {
-  res.json({
-    success: {
-      message: "Cats",
+export const cats = (_req: Request, res: Response) => {
+  res.render("pages/page", {
+    menu: createMenuObject("cats"),
+    banner: {
+      title: "Todos os gatos",
+      background: "banner_cat.jpg",
     },
   });
 };
-export const fishes = (req: Request, res: Response) => {
-  res.json({
-    success: {
-      message: "Fishes",
+export const fishes = (_req: Request, res: Response) => {
+  res.render("pages/page", {
+    menu: createMenuObject("fishes"),
+    banner: {
+      title: "Todos os peixes",
+      background: "banner_fish.jpg",
     },
   });
 };
